ae69c84d5d194bc069cd4ee8630eb18eb4a7bd74
Colin Carter <ccarter@skyscapecloud.com>
2017-06-15 16:33:34 +0100

    es6-ify and prettify

--- public/javascripts/quickword.js
+++ public/javascripts/quickword.js
@@ -1,147 +1,153 @@
-window.quickWord = function() {
-    var canvas, context,
-        t, st, ft,          // Timeouts
-        words,
-        maxWords,
-        x           = -30,
-        delay       = 30,
-        currentWord = 0,
-        success     = false,
-        successCount=0,
-        wordPos     = 0;
+(() => {
+  const quickWord = () => {
+    let canvas,
+      context,
+      t,
+      st,
+      ft, // Timeouts
+      words,
+      maxWords,
+      x = -30,
+      delay = 30,
+      currentWord = 0,
+      success = false,
+      successCount = 0,
+      wordPos = 0;
 
-    var nextWord = function() {
-        x         = -10;
-        success   = false;
-        wordPos     = 0;
+    const nextWord = () => {
+      x = -10;
+      success = false;
+      wordPos = 0;
 
-        if(++currentWord >= maxWords) {
-           currentWord = 0;
-       }
+      if (++currentWord >= maxWords) {
+        currentWord = 0;
+      }
 
-       setDelayForWord();
-   };
+      setDelayForWord();
+    };
 
-    var setDelayForWord = function() {
-        if(words[currentWord].length > 6) {
-            delay = 30;
-        } else {
-            delay = 20;
+    const setDelayForWord = () => {
+      if (words[currentWord].length > 6) {
+        delay = 30;
+      } else {
+        delay = 20;
+      }
+    };
+
+    const clearCanvas = () => {
+      context.clearRect(0, 0, canvas.width, canvas.height);
+      context.save();
+    };
+
+    const scrollText = () => {
+      x++;
+
+      clearCanvas();
+      context.translate(x, canvas.height / 2);
+      context.fillText(words[currentWord], 0, 0);
+      context.restore();
+
+      // If the end of the word reaches the right-hand edge of the canvas
+      // then reset things
+      if (
+        x + context.measureText(words[currentWord]).width >=
+        canvas.width + 30
+      ) {
+        nextWord();
+      }
+
+      t = setTimeout(() => {
+        scrollText();
+      }, delay);
+    };
+
+    const updateSuccessDisplay = () => {
+      document.getElementById("success").innerHTML = "Wins: " + successCount;
+    };
+
+    const showSuccess = () => {
+      clearTimeout(t);
+      clearTimeout(st);
+
+      successCount++;
+
+      clearCanvas();
+      updateSuccessDisplay();
+
+      context.fillText("Success", canvas.width / 2, canvas.height / 2);
+
+      st = setTimeout(() => {
+        nextWord();
+        scrollText();
+      }, 500);
+    };
+
+    const showFail = () => {
+      clearTimeout(t);
+      clearTimeout(ft);
+
+      clearCanvas();
+      context.fillText("Fail", canvas.width / 2, canvas.height / 2);
+
+      ft = setTimeout(function() {
+        nextWord();
+        scrollText();
+      }, 500);
+    };
+
+    const onKeyPress = e => {
+      const keyChar = String.fromCharCode(e.keyCode || e.charCode);
+
+      if (words[currentWord].charAt(wordPos) == keyChar) {
+        wordPos++;
+        success = true;
+      } else {
+        success = false;
+        showFail();
+      }
+
+      if (success && wordPos >= words[currentWord].length) {
+        showSuccess();
+      }
+
+      return false;
+    };
+
+    const initKeyboardEvents = () => {
+      window.onkeypress = onKeyPress;
+    };
+
+    const withWords = callback => {
+      const xmlHttp = new XMLHttpRequest();
+      xmlHttp.onreadystatechange = function() {
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+          words = JSON.parse(xmlHttp.responseText);
+          maxWords = words.length;
+          callback();
         }
-   };
-
-   var clearCanvas = function() {
-        context.clearRect(0, 0, canvas.width, canvas.height);
-        context.save();
+      };
+      xmlHttp.open("GET", "words", false);
+      xmlHttp.send();
     };
 
-    var scrollText = function() {
-        x++;
+    withWords(function() {
+      canvas = document.getElementById("can");
+      context = canvas.getContext("2d");
 
-        clearCanvas();
-        context.translate(x, canvas.height/2);
-        context.fillText(words[currentWord], 0, 0);
-        context.restore();
+      context.fillStyle = "blue";
+      context.font = "18px Verdana";
+      context.textAlign = "center";
+      context.textBaseLine = "middle";
 
-        // If the end of the word reaches the right-hand edge of the canvas
-        // then reset things
-        if((x + context.measureText(words[currentWord]).width) >= canvas.width+30) {
-            nextWord();
-        }
+      updateSuccessDisplay();
 
-        t = setTimeout(function() {
-            scrollText();
-        }, delay);
-    };
+      initKeyboardEvents();
 
-    var updateSuccessDisplay = function() {
-        document.getElementById('success').innerHTML = "Wins: " + successCount;
-    }
+      setDelayForWord();
 
-    var showSuccess = function() {
-        clearTimeout(t);
-        clearTimeout(st);
+      scrollText();
+    });
+  };
 
-        successCount++;
-
-        clearCanvas();
-        updateSuccessDisplay();
-
-        context.fillText("Success", canvas.width/2, canvas.height/2);
-
-        st = setTimeout(function() {
-            nextWord();
-            scrollText();
-        }, 500);
-    };
-
-    var showFail = function() {
-        clearTimeout(t);
-        clearTimeout(ft);
-
-        clearCanvas();
-        context.fillText("Fail", canvas.width/2, canvas.height/2);
-
-        ft = setTimeout(function() {
-            nextWord();
-            scrollText();
-        }, 500);
-    };
-
-    var onKeyPress = function(e) {
-        var keyChar = String.fromCharCode(e.keyCode || e.charCode);
-
-        if(words[currentWord].charAt(wordPos) == keyChar) {
-            wordPos++;
-            success = true;
-        } else {
-            success = false;
-            showFail();
-        }
-
-        if(success && wordPos >= words[currentWord].length) {
-            showSuccess();
-        }
-
-        return false;
-    };
-
-    var initKeyboardEvents = function() {
-        window.onkeypress = onKeyPress;
-    };
-
-    var withWords = function(callback) {
-        var xmlHttp = new XMLHttpRequest();
-        xmlHttp.onreadystatechange = function() {
-            if(xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-                words = JSON.parse(xmlHttp.responseText);
-                maxWords = words.length;
-                callback();
-            }
-        }
-        xmlHttp.open("GET", "words", false);
-        xmlHttp.send();
-    };
-
-    return {
-        run: function() {
-            withWords(function() {
-                canvas  = document.getElementById('can');
-                context = canvas.getContext('2d');
-
-                context.fillStyle       = 'blue';
-                context.font            = "18px Verdana";
-                context.textAlign       = "center";
-                context.textBaseLine    = "middle";
-
-                updateSuccessDisplay();
-
-                initKeyboardEvents();
-
-                setDelayForWord();
-
-                scrollText();
-            });
-        }
-    }
-}();
+  document.addEventListener("DOMContentLoaded", quickWord);
+})();
--- routes/index.js
+++ routes/index.js
@@ -1,55 +1,53 @@
-'use strict'
+"use strict";
 
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const fs = require('fs');
-const file = require('../lib/file');
-const shuffle = require('shuffle-array');
+const fs = require("fs");
+const file = require("../lib/file");
+const shuffle = require("shuffle-array");
 
-const USR_SHARE_WORDS = '/usr/share/dict/words';
-const USR_DICT_WORDS  =  '/usr/dict/words';
+const USR_SHARE_WORDS = "/usr/share/dict/words";
+const USR_DICT_WORDS = "/usr/dict/words";
 
 let wordListFile = findWordList();
 let wordList = [];
 
 function findWordList() {
-    if(file.exists(USR_SHARE_WORDS)) {
-        return USR_SHARE_WORDS;
-    }
+  if (file.exists(USR_SHARE_WORDS)) {
+    return USR_SHARE_WORDS;
+  }
 
-    if(file.exists(USR_DICT_WORDS)) {
-        return USR_DICT_WORDS;
-    }
+  if (file.exists(USR_DICT_WORDS)) {
+    return USR_DICT_WORDS;
+  }
 
-    throw 'No word list found';
+  throw "No word list found";
 }
 
 function parseFiles(words) {
-    let parsedWords = words.split("\n")
-        .map((w) => {
-            return w.trim();
-        })
-        .filter((w) => {
-            return w.length > 0;
-        })
-    wordList = shuffle(parsedWords);
+  let parsedWords = words
+    .split("\n")
+    .map(w => {
+      return w.trim();
+    })
+    .filter(w => {
+      return w.length > 0;
+    });
+  wordList = shuffle(parsedWords);
 }
 
-
-file.readFile(wordListFile)
-    .then(parseFiles)
-    .catch((err) => {
-        throw 'error loading files';
-    });
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Quickword' });
+file.readFile(wordListFile).then(parseFiles).catch(err => {
+  throw "error loading files";
 });
 
-router.get('/words', function(req, res, next) {
-    res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify(wordList));
+/* GET home page. */
+router.get("/", function(req, res, next) {
+  res.render("index", { title: "Quickword" });
+});
+
+router.get("/words", function(req, res, next) {
+  res.setHeader("Content-Type", "application/json");
+  res.send(JSON.stringify(wordList));
 });
 
 module.exports = router;
